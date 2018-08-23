89c64e56f158fcddde0699549e8a6f564613ad3c
hshaw785 <haley.shaw@huskers.unl.edu>
2018-08-23 14:19:01 -0500

    Print Program, name and major

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Julie Asbury and Haley Shaw
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Haley Shaw, Computer Engineering\n");
+
+  return 0;
+}
